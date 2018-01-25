eddec84c0bc708e948c8984905558945c633b18f
Yawar Amin <yawar.amin@gmail.com>
2018-01-25 15:49:32 -0500

    Add some Node bindings

--- src/Yawaramin_BsWebapi_Node.ml
+++ src/Yawaramin_BsWebapi_Node.ml
@@ -4,6 +4,12 @@ type 'a t = ([> intf] as 'a) Yawaramin_BsWebapi_Common.t
 external baseURI: 'a t -> string = "" [@@bs.get]
 external childNodes: 'a t -> 'a t array = "" [@@bs.get]
 external firstChild: 'a t -> 'a t = "" [@@bs.get]
+external lastChild: 'a t -> 'a t = "" [@@bs.get]
+external nextSibling: 'a t -> 'a t = "" [@@bs.get]
+external nodeName: 'a t -> string = "" [@@bs.get]
+external nodeType: 'a t -> int = "" [@@bs.get]
+external nodeValue: 'a t -> string = "" [@@bs.get]
+external setNodeValue: 'a t -> string -> unit = "" [@@bs.set]
 
 let cast t = t
   |> Yawaramin_BsWebapi_Common.unsafeCoerce
