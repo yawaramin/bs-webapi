3beddc677b9fc2760afaf8e66d5095c2a23529a0
Yawar Amin <yawar.amin@gmail.com>
2018-01-25 18:14:28 -0500

    Clarify links between return values and associated modules

--- src/Yawaramin_BsWebapi_Node.mli
+++ src/Yawaramin_BsWebapi_Node.mli
@@ -1,5 +1,15 @@
 (**)
 
+type intf = [Yawaramin_BsWebapi_EventTarget.intf | `node]
+type 'a t = ([> intf] as 'a) Yawaramin_BsWebapi_Common.t
+
+external baseURI: 'a t -> string = "" [@@bs.get]
+external childNodes: 'a t -> 'b t array = "" [@@bs.get]
+external firstChild: 'a t -> 'b t Js.nullable = "" [@@bs.get]
+external lastChild: 'a t -> 'b t Js.nullable = "" [@@bs.get]
+external nextSibling: 'a t -> 'b t Js.nullable = "" [@@bs.get]
+external nodeName: 'a t -> string = "" [@@bs.get]
+
 module Type: sig
   val element: int
   val text: int
@@ -10,24 +20,7 @@ module Type: sig
   val documentFragment: int
 end
 
-module DocumentPosition: sig
-  val disconnected: int
-  val preceding: int
-  val following: int
-  val contains: int
-  val containedBy: int
-  val implementationSpecific: int
-end
-
-type intf = [Yawaramin_BsWebapi_EventTarget.intf | `node]
-type 'a t = ([> intf] as 'a) Yawaramin_BsWebapi_Common.t
-
-external baseURI: 'a t -> string = "" [@@bs.get]
-external childNodes: 'a t -> 'b t array = "" [@@bs.get]
-external firstChild: 'a t -> 'b t Js.nullable = "" [@@bs.get]
-external lastChild: 'a t -> 'b t Js.nullable = "" [@@bs.get]
-external nextSibling: 'a t -> 'b t Js.nullable = "" [@@bs.get]
-external nodeName: 'a t -> string = "" [@@bs.get]
+(** Possible return values are in {!module:Yawaramin_BsWebapi_Node.Type}. *)
 external nodeType: 'a t -> int = "" [@@bs.get]
 external nodeValue: 'a t -> string = "" [@@bs.get]
 external setNodeValue: 'a t -> string -> unit = "nodeValue" [@@bs.set]
@@ -44,6 +37,18 @@ external setTextContent: 'a t -> string -> unit =
 
 external appendChild: 'b t -> 'b t = "" [@@bs.send.pipe: 'a t]
 external cloneNode: ?deep:Js.boolean -> 'a t = "" [@@bs.send.pipe: 'a t]
+
+module DocumentPosition: sig
+  val disconnected: int
+  val preceding: int
+  val following: int
+  val contains: int
+  val containedBy: int
+  val implementationSpecific: int
+end
+
+(** Possible return values are in
+    {!module:Yawaramin_BsWebapi_Node.DocumentPosition}. *)
 external compareDocumentPosition: 'b t -> int = "" [@@bs.send.pipe: 'a t]
 external contains: 'b t -> Js.boolean = "" [@@bs.send.pipe: 'a t]
 external hasChildNodes: 'a t -> Js.boolean = "" [@@bs.send]
