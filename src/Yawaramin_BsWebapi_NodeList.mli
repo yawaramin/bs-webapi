f648450bbe80a35f9c36e55b6fd635392b7d6ab8
Yawar Amin <yawar.amin@gmail.com>
2018-01-27 18:17:01 -0500

    Warn about proper usage of NodeList.forEach
    Also give correct example usage.

--- src/Yawaramin_BsWebapi_NodeList.mli
+++ src/Yawaramin_BsWebapi_NodeList.mli
@@ -10,7 +10,17 @@ external length: t -> int = "" [@@bs.get]
 external entries: t -> (int * 'a node) iterator = "" [@@bs.send]
 
 (** [\[\@\@bs.send.pipe: t\]] (last argument is a [t] value). Also,
-    [callback] must be a BuckleScript-uncurried function with [\[\@bs\]]. *)
+    [callback] must be a BuckleScript-uncurried function with [\[\@bs\]].
+
+    Careful with labelling the arguments. If you forget, the type error
+    message won't mention a missing label because it will assume the
+    unlabelled argument is the [t] (last) parameter. Here's a correct
+    usage: {[
+module Web = Yawaramin_BsWebapi
+let test (elem: 'a Web.Element.supertype) = elem
+  |> Web.ParentNode.querySelectorAll "p"
+  |> Web.NodeList.forEach ~callback:(fun \[\@bs\] node _ _ -> Js.log node)
+    ]} *)
 external forEach: callback:('a node -> int -> t -> unit [@bs]) -> ?thisArg:t -> unit = "" [@@bs.send.pipe: t]
 external keys: t -> int iterator = "" [@@bs.send]
 external values: t -> 'a node iterator = "" [@@bs.send]
--- src/Yawaramin_BsWebapi_Test.ml
+++ src/Yawaramin_BsWebapi_Test.ml
@@ -1,3 +1,4 @@
 module Web = Yawaramin_BsWebapi
-let test (elem: 'a Web.Element.supertype) =
-  Web.ParentNode.children elem
+let test (elem: 'a Web.Element.supertype) = elem
+  |> Web.ParentNode.querySelectorAll "p"
+  |> Web.NodeList.forEach ~callback:(fun [@bs] node _ _ -> Js.log node)
